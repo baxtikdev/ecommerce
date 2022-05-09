1e23f3998b673024d2e3a46c3c3533d22a49d8ab
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-05-09 20:07:28 +0500

    create model

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls'))
 ]
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='Category',null=True, blank=True)
+
+    @property
+    def imageURL(self):
+        try:
+            return self.image.url
+        except:
+            pass
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    choice = (
+        ('Select a size','Select a size'),
+        ('Small','Small'),
+        ('Medium','Medium'),
+        ('Large','Large'),
+        ('Extra large','Extra large'),
+    )
+
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='Product')
+    description = models.TextField()
+    color = models.ManyToManyField(Color)
+    size = models.CharField(max_length=30,choices=choice)
+    quantity = models.IntegerField(default=1)
+    reyting = models.FloatField(default=0)
+    discount = models.FloatField(default=0)
+
+    @property
+    def imageURL(self):
+        try:
+            return self.image.url
+        except:
+            pass
+
+    def __str__(self):
+        return self.name
+
+class Card(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+    total_price = models.FloatField(default=0)
+
+    @property
+    def total(self):
+        self.total_price = sum([i.price*(1-i.discount/100) for i in self.product.all()])
+        self.save()
+
+
+    def __str__(self):
+        return f"{self.user.username} | {self.total_price}"
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"{self.id} | {self.user.username}"
+
+class SaleHistory(models.Model):
+    payment = (
+        ('Humo', 'Humo'),
+        ('UzCard', 'UzCard'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ManyToManyField(Product)
+    payment_type = models.CharField(max_length=20,choices=payment)
+    total_price = models.FloatField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id} | {self.user.username} | {self.time}"
