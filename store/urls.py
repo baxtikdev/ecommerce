e5a3dd74cce4e16652d0b262dd0701a24926c074
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-05-11 21:02:06 +0500

    add

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('product_info/',product_info,name='product_info'),
+    path('cart/',cart,name='cart'),
+    path('wishlist/',wishlist,name='wishlist'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def product_info(request):
+    return render(request, 'product-extended.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
