d4a43255d3d9023135bb2dae6fcd3e5a1ad4581a
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-05-25 19:35:45 +0500

    add

--- store/contex_pro.py
+++ store/contex_pro.py
@@ -1,10 +1,16 @@
 from .models import Card,Category
 
 def context_process(request):
-    products = Card.objects.get(user=request.user).product.all()
-    context = {
-        'categories': Category.objects.all(),
-        'products': products,
-        'total': sum(products.values_list('price',flat=True))
-    }
+    print(request.user)
+    try:
+        products = Card.objects.get(user=request.user).product.all()
+        context = {
+            'categories': Category.objects.all(),
+            'products': products,
+            'total': sum(products.values_list('price',flat=True))
+        }
+    except:
+        context = {
+            'categories': Category.objects.all(),
+        }
     return context
